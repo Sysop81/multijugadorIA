78efa18b5137b4d5b1320ac3acb8462865f7c16e
José Ramón López <jose.rlg27@gmail.com>
2024-10-28 22:55:48 +0100

    Working in example Network Manager UT-1

--- Example/Assets/Scripts/PlayerControls.cs
+++ Example/Assets/Scripts/PlayerControls.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerControls : MonoBehaviour
+public class PlayerControls : NetworkBehaviour /*MonoBehaviour*/
 {
     [SerializeField] private float speed = 0.5f;
 
@@ -15,10 +16,31 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        if(!IsOwner) return;
+        
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
         Vector3 moveDirection = new Vector3(xInput, 0, yInput).normalized;
         transform.Translate(speed * Time.deltaTime * moveDirection);
+        
+        //UpdatePositionServerRpc(transform.position);
+        
+    }
+    
+    [ServerRpc]
+    private void UpdatePositionServerRpc(Vector3 newPosition)
+    {
+        // Update clients positions
+        UpdatePositionClientRpc(newPosition);
+    }
+    
+    [ClientRpc]
+    private void UpdatePositionClientRpc(Vector3 newPosition)
+    {
+        if (IsOwner) return; 
 
+        // Update all clients
+        transform.position = newPosition;
     }
 }
